525ba41c4fe85e5bb7e0727993b93dabb6bf7673
happyendik <vlom4ek@gmail.com>
2017-07-24 12:34:53 +0300

    third commit

--- InwidgetWrapper.php
+++ InwidgetWrapper.php
@@ -1,8 +1,15 @@
 <?php
 
-namespace happyendik\inwidget;
+namespace happyendik;
 
-class InstagramWidget extends \yii\base\Widget
+class InwidgetWrapper extends \yii\base\Widget
 {
-
+    public function run()
+    {
+        ?>
+        <!-- Без профиля -->
+        <iframe src='/inwidget/index.php?toolbar=false' scrolling='no'
+                frameborder='no' style='border:none;width:260px;height:320px;overflow:hidden;'></iframe>
+        <?php
+    }
 }
